89d88e6446a0cd216ca424f09d60fcb5bcbf9ec8
ChasingRain <junpeng94@gmail.com>
2022-05-16 17:09:40 +0800

    added conditional rendering and decoupled button function

--- incrementer/src/App.js
+++ incrementer/src/App.js
@@ -1,30 +1,72 @@
 import { useState } from "react"
 
-const Display = ({counter}) => {
-  return(
-    <p>{counter}</p>
-  )
-}
-
 const Button = ({action,sign}) => {
   return(
     <button onClick={action}>{sign}</button>
   )
 }
-const App = (props) => {
-  const [counter, setCounter] = useState(0)
-  const increaseByOne = () => (setCounter(counter+1))
-  const decreaseByOne = () => {if(counter>0) setCounter(counter-1)}
-  const resetToZero = () => (setCounter(0))
 
-  console.log('rendering...', counter)
+const History = (props) => {
+  if (props.allClicks.length === 0){
+    return(
+    <div>
+      the app is used by pressing the buttons
+    </div>
+    )
+  }
+  else{
+    return(
+      <div>
+      button press history: {props.allClicks.join(" ")}
+      </div>
+    )
+  }
+}
+
+const App = (props) => {
+  // const [counter, setCounter] = useState(0)
+  const[click,setClicks] = useState({left:0,right:0})
+  const[allClicks,setAll] = useState([])
+
+  const handleLeftClicks = () => {
+    const newClicks = {
+      ...click,
+      left: click.left + 1,
+      
+    }
+    setAll(allClicks.concat("L"))
+    setClicks(newClicks)
+  }
+
+  const handleRightClicks = () => {
+    const newClicks = {
+      ...click,
+      right: click.right + 1
+    }
+    setAll(allClicks.concat("R"))
+    setClicks(newClicks)
+  }
+
+  const resetToZero = () => {
+    const resetClicks = {
+      left: 0,
+      right: 0
+    }
+    setClicks(resetClicks)
+    setAll([])
+  }
+
+
+  // console.log('rendering...', counter)
 
   return (
     <div>
-      <Display counter = {counter}/> 
-      <Button action={increaseByOne} sign="+"/>
-      <Button action={decreaseByOne} sign="-"/>
-      <Button action={resetToZero} sign="Reset"/>
+      {click.left}
+      <Button action={handleLeftClicks} sign="left"/>
+      <Button action={handleRightClicks} sign="right"/>
+      {click.right}<br></br>
+      <Button action={resetToZero} sign="Reset"/><br></br>
+      <History allClicks={allClicks}/>
     </div>
     
   )
