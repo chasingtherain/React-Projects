4cfb8ae0442e989f3b18ab5811804867ac561153
ChasingRain <junpeng94@gmail.com>
2022-05-20 12:15:23 +0800

    initial commit

---
+++ country-finder/src/App.js
@@ -0,0 +1,12 @@
+// import { useState, useEffect } from 'react'
+// import axios from 'axios'
+
+const App = () => {
+  return (
+    <div>
+      <h2>Country Finder</h2>
+    </div>
+  )
+}
+
+export default App
